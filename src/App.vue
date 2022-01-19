c14b59541cefbcab7882cd5a49bc6e582469f3f3
zink wu <wugu2599l@gmail.com>
2022-01-20 02:18:02 +0800

    add element ui

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,31 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">Home</router-link>|
     <router-link to="/about">About</router-link>
+    <el-row>
+      <el-button>button</el-button>
+      <el-button :icon="Search" circle></el-button>
+      <el-button type="primary" :icon="Edit" circle></el-button>
+      <el-button type="success" :icon="Check" circle></el-button>
+      <el-button type="info" :icon="Message" circle></el-button>
+      <el-button type="warning" :icon="Star" circle></el-button>
+      <el-button type="danger" :icon="Delete" circle></el-button>
+    </el-row>
   </div>
-  <router-view/>
+  <router-view />
 </template>
+
+<script lang="ts" setup>
+import {
+  Search,
+  Edit,
+  Check,
+  Message,
+  Star,
+  Delete,
+} from '@element-plus/icons-vue'
+
+</script>
 
 <style lang="scss">
 #app {
